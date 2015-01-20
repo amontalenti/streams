776004900ccef0bbdf6ab9d19cb7c20a7d0090d2
Andrew Montalenti <andrew@parsely.com>
2015-01-20 16:54:44 -0500

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-ssh cogtree@www.cogtree.com mkdir -p /data/vhosts/parsely.com/slides/logs
-rsync -Pavz --exclude=.git ./_build/slides/ cogtree@www.cogtree.com:/data/vhosts/parsely.com/slides/logs
-rsync -Pavz --exclude=.git ./_build/html/ cogtree@www.cogtree.com:/data/vhosts/parsely.com/slides/logs/notes/
+ssh cogtree@www.cogtree.com mkdir -p /data/vhosts/parsely.com/slides/streamparse
+rsync -Pavz --exclude=.git ./_build/slides/ cogtree@www.cogtree.com:/data/vhosts/parsely.com/slides/streamparse
+rsync -Pavz --exclude=.git ./_build/html/ cogtree@www.cogtree.com:/data/vhosts/parsely.com/slides/streamparse/notes/
